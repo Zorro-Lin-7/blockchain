a790bba7709305a6703b18c3106682f08612f465
linzhun <linzhun7@gmail.com>
2018-06-02 18:33:43 +0800

    Testing 'createNewTransaction' method

--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,11 @@ const Blockchain = require('./blockchain')
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2245, 'o08qg3', '055wrjl2') //随便输入3个参数
+
+bitcoin.createNewTransaction(100, 'Tom_Address', 'Jerry_Address')
+
 bitcoin.createNewBlock(412, 'g346h', '5622hefdf') //随便输入3个参数
-bitcoin.createNewBlock(2245, '26gadg8', '24thdsh6') //随便输入3个参数
+// bitcoin.createNewBlock(2245, '26gadg8', '24thdsh6') //随便输入3个参数
+
 
 console.log(bitcoin)
