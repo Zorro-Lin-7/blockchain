cc5e53cf502d56eadbd874767dc0af7fe0c03f30
linzhun <linzhun7@gmail.com>
2018-06-11 08:18:50 +0800

    Building The API Foundation

--- dev/api.js
+++ dev/api.js
@@ -1,9 +1,30 @@
 var express = require('express')
-//creating an app with this app we can handle different endpoints or different routes
 var app = express()
 
-app.get('/', function (req, res) {  //we have a "get" endpoint which is just "/".
-  res.send('Hello JavaScript Code')  // with this endpoint we are sending back the response of hello world.
+// First endpoint is going to be "/blockchain"
+// what this endpoint is going to do is
+// when we hit it it is simply going to send back to us our entire blockchain
+app.get('/blockchian', function (req, res) {
+
 })
 
-app.listen(3000)  // this whole server is listening on port 3000.
+
+// The next point that we are going to make is going to be post to "/transaction"
+// this is the endpoint that we will hit to create a new transaction on our block chain.
+app.post('/transaction', function (req, res) {
+
+})
+
+
+// the third point that we are going to create will be app that get to "/mine"
+// and put our callback function in here with the request and response.
+// And what this '/mine' end point will do is
+// it will mine a new block for us or create a new block
+app.get('/mine', function (req, res) {
+
+})
+
+// 添加一个函数，已观察服务器正常运行ing
+app.listen(3000, function () {
+  console.log('Listening on port 3000...')
+})
