6606c5faad94069cae109607b064b958efe4b86d
linzhun <linzhun7@gmail.com>
2018-06-26 07:22:14 +0800

    Building GET transaction/transactionId Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -271,7 +271,12 @@ app.get('/block/:blockHash', function(req, res) {  // lockalhost:3001/block/EWON
 
 
 app.get('/transaction/:transactionId', function(req, res) {
-
+    const transactionId = req.params.transactionId
+    const transactionData = bitcoin.getTransaction(transactionId)
+    res.json({
+        transaction: transactionData.transaction,
+        block: transactionData.block
+    })
 })
 
 
